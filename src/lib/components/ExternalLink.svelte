98ad806664ef977c1e2fa2529f5b0bb77af3a8c1
LeTamanoir <martin.saldinger@gmail.com>
2023-09-13 22:29:45 +0200

    new link component

---
+++ src/lib/components/ExternalLink.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import network from "$lib/store/network";
+
+  export let href = "";
+  export let variant: "beaconcha.in" | "etherscan.io" = "beaconcha.in";
+
+  let clazz = "";
+  export { clazz as class };
+
+  let baseUrl = "";
+
+  $: {
+    if (variant === "beaconcha.in") {
+      baseUrl = `https://${$network === "goerli" ? "prater." : ""}beaconcha.in`;
+    } else if (variant === "etherscan.io") {
+      baseUrl = `https://${$network === "goerli" ? "goerli." : ""}etherscan.io`;
+    }
+  }
+</script>
+
+<a target="_blank" class="underline {clazz}" href={new URL(href, baseUrl).toString()}>
+  <slot />
+</a>
