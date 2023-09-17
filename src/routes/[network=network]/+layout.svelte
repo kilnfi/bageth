66f9693e1cf24baf247fad60ddc52ab64fb2974d
LeTamanoir <martin.saldinger@gmail.com>
2023-09-17 13:18:56 +0200

    add persistent search params improve efficiency when preloading page (e.g. link hovering)

--- src/routes/[network=network]/+layout.svelte
+++ src/routes/[network=network]/+layout.svelte
@@ -1,26 +1,18 @@
 <script lang="ts">
-  import { beforeNavigate, goto } from "$app/navigation";
   import { navigating, page } from "$app/stores";
   import SearchForm from "$lib/components/SearchForm.svelte";
-
-  const tabs = ["stakes", "rewards", "operations"] as const;
+  import persistedSearchParams from "$lib/store/persistedSearchParams";
 
   $: search = $page.url.searchParams.get("search") ?? "";
   $: loading = $navigating?.type === "form";
 
-  beforeNavigate(({ from, to, cancel }) => {
-    if (!to || !from) return;
-
-    // if we exit the (search) layout we don't persist any search query
-    if (tabs.every((tab) => !to.route.id?.includes(`/${tab}`))) return;
-
-    // persist the search query
-    if (from.url.searchParams.has("search") && !to.url.searchParams.has("search")) {
-      cancel();
-      to.url.searchParams.set("search", from.url.searchParams.get("search")!);
-      goto(to.url);
-    }
-  });
+  $: {
+    // persist the search for all routes
+    persistedSearchParams.update((u) => {
+      u.set("search", search);
+      return u;
+    });
+  }
 </script>
 
 <SearchForm {loading} {search} />
