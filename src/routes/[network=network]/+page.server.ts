4b728ffac44bb21d3ee4ae50abe2906c3484c651
LeTamanoir <martin.saldinger@gmail.com>
2023-11-12 18:52:09 +0100

    Add server-side data fetching for rewards, stats, and price.

--- src/routes/[network=network]/+page.server.ts
+++ src/routes/[network=network]/+page.server.ts
@@ -1,6 +1,19 @@
-import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
+import { createServerClient } from "$lib/server/fetcher";
+import { subDays } from "date-fns";
+import { formatDate } from "$lib/utils";
 
-export const load = (async () => {
-  throw redirect(301, "/");
+export const load = (async ({ fetch, params }) => {
+  const network = params.network;
+  const fetcher = createServerClient(network, fetch);
+
+  const rewards = await fetcher.GET("/v1/eth/rewards", { params: { query: { scope: "network" } } });
+
+  const stats = await fetcher.GET("/v1/eth/rewards", {
+    params: { query: { scope: "network", start_date: formatDate(subDays(new Date(), 1)) } },
+  });
+
+  const price = await fetcher.GET("/v1/eth/network-stats", {});
+
+  return { rewards: rewards.data, stats: stats.data, price: price.data?.data?.eth_price_usd };
 }) satisfies PageServerLoad;
