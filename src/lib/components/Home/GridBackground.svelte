2446e8edd482006fc544f1038b326533c818e898
LeTamanoir <martin.saldinger@gmail.com>
2023-11-12 18:50:12 +0100

    Add new components and styles for home page

---
+++ src/lib/components/Home/GridBackground.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Gradients from "./Gradients.svelte";
+</script>
+
+<div class="w-full grow relative overflow-hidden">
+  <slot />
+
+  <Gradients />
+</div>
+
+<style>
+  div {
+    background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, #131313 100%);
+  }
+
+  div::before {
+    @apply absolute content-[''] top-0 left-0 right-0 bottom-0;
+    border-bottom: 1px solid #2f2c27;
+    border-right: 1px solid #2f2c27;
+    background-image: url("/bg-cell.svg");
+    background-repeat: repeat;
+    z-index: -1;
+  }
+</style>
---
+++ src/lib/components/Home/Hero.svelte
@@ -0,0 +1,13 @@
+<section class="flex flex-col items-center gap-y-4 mt-20 lg:mt-28 px-4 mb-10">
+  <h2 class="font-neue text-[48px] lg:text-[64px] font-bold leading-[110%] sm:text-center">
+    <span class="bg-clip-text text-transparent bg-[linear-gradient(115deg,#FFA800_7.74%,#B0B8FF_56.72%)]">
+      ETH Explorer
+    </span><br />
+    <span class="text-white"> for the beacon chain </span>
+  </h2>
+
+  <p class="text-dark-lighter sm:text-center text-base">
+    Bageth.xyz is a user-friendly interface for Kiln Connect, enabling easy validator <br />
+    queries and real-time staking info in your browser.
+  </p>
+</section>
