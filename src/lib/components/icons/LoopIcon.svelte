7677a42ae3eb28f199b0d3d36a9a5e426d392893
LeTamanoir <martin.saldinger@gmail.com>
2023-11-12 18:50:32 +0100

    Add icons

---
+++ src/lib/components/icons/LoopIcon.svelte
@@ -0,0 +1,16 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
+  <path
+    d="M23.3331 23.3333L19.1055 19.1057"
+    stroke="white"
+    stroke-width="1.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M4.6665 13.125C4.6665 17.7964 8.45343 21.5834 13.1248 21.5834C17.7962 21.5834 21.5832 17.7964 21.5832 13.125C21.5832 8.45361 17.7962 4.66669 13.1248 4.66669V4.66669C8.45357 4.66703 4.66685 8.45375 4.6665 13.125"
+    stroke="white"
+    stroke-width="1.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</svg>
