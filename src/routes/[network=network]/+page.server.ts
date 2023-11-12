b4c643c9a1e9d4c733cf8da62e97f4ac9209e4da
LeTamanoir <martin.saldinger@gmail.com>
2023-11-12 18:58:37 +0100

    Add cache-control header to server response

--- src/routes/[network=network]/+page.server.ts
+++ src/routes/[network=network]/+page.server.ts
@@ -3,7 +3,7 @@ import { createServerClient } from "$lib/server/fetcher";
 import { subDays } from "date-fns";
 import { formatDate } from "$lib/utils";
 
-export const load = (async ({ fetch, params }) => {
+export const load = (async ({ fetch, params, setHeaders }) => {
   const network = params.network;
   const fetcher = createServerClient(network, fetch);
 
@@ -14,6 +14,10 @@ export const load = (async ({ fetch, params }) => {
   });
 
   const price = await fetcher.GET("/v1/eth/network-stats", {});
+
+  setHeaders({
+    "cache-control": `max-age=${60 * 60 * 12}`,
+  });
 
   return { rewards: rewards.data, stats: stats.data, price: price.data?.data?.eth_price_usd };
 }) satisfies PageServerLoad;
